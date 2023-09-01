59ee868961da1762b740aba76a881a5f754cf988
adjust4u <sdzpoya12@gmail.com>
2023-09-01 15:32:31 +0330

    order project in src folder add background change litle style main.css

--- src/script.js
+++ src/script.js
@@ -1,37 +1,37 @@
 let interval = null;
 
-function getImages(){
+function getImages() {
   const images = document.querySelectorAll("img");
-  return images
+  return images;
 }
 
 function blurImage() {
-  const images = getImages()
+  const images = getImages();
   images.forEach((image) => {
     image.style.filter = "blur(20px)";
   });
-};
+}
 
-function unBlurImage(){
+function unBlurImage() {
   clearInterval(interval);
   const images = getImages();
   images.forEach((image) => {
-    image.style.filter = "blur(0px)"
+    image.style.filter = "blur(0px)";
   });
-};
-
+}
 
 // Listen for messages from the popup
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.action === "contentFunction") {
     myContentFunction();
   }
   switch (message.action) {
-    case 'visible':
+    case "visible":
       unBlurImage();
       break;
-    case 'unvisible':
-      interval = setInterval(blurImage,1000);
+    case "unvisible":
+      clearInterval(interval);
+      interval = setInterval(blurImage, 1000);
       break;
     default:
       break;
